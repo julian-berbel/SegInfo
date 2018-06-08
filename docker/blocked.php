af9f13c250b3be360676d7f37b9a900ec494831c
Julian Berbel Alt <julianberbel@hotmail.com>
2018-06-08 00:03:32 -0300

    Agrego fix 2

---
+++ docker/blocked.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1>Bad Request</h1>
+
+        <h2>El pedido HTTP parece un ataque</h2>
+
+        <p>Tu pedido HTTP fue rechazado debido a que parecia ser un ataque al servidor. Si crees que se trata de un error, escribinos a blahblah@gmail.com
+    </body>
+</html>
